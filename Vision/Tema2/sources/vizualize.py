e2b3ee94aa54cad130c297921da7380ba0d73ec7
Theodor Moroianu <theodor.moroianu@gmail.com>
2022-01-09 12:28:35 +0200

    Add examen

--- Vision/Tema2/sources/vizualize.py
+++ Vision/Tema2/sources/vizualize.py
@@ -7,44 +7,32 @@ import os
 import random
 import sources.project_utils as project_utils
 
+
+def read_images(folder_path):
+    # (filename, image)
+    images = []
+    
+    # read all images from folder
+    for filename in os.listdir(folder_path):
+        img = plt.imread(folder_path + filename)
+        images.append(img)
+
+    return images
+
 def vizualize(input_path):
     """
     prints ground truth and predictions
     """
-    lines = open(input_path + "simpsons_validare.txt", "r").readlines()
-
-    line = lines[random.randint(0, len(lines) - 1)]
     
-    filename, xmin, ymin, xmax, ymax, face_name = line.split()
-    xmin = int(xmin)
-    xmax = int(xmax)
-    ymin = int(ymin)
-    ymax = int(ymax)
-    
-    img = plt.imread(input_path + "simpsons_validare/" + filename)
-    predictions = sliding_window.find_faces(img)
+    imgs = read_images(input_path)
 
-    cv.rectangle(img, (xmin, ymin), (xmax, ymax), (0, 0, 255), thickness=3)
-    cv.putText(img, "GT", (xmin, ymin),
-                cv.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 0), 1)
+    for im in imgs:
+        preds = sliding_window.find_faces(im)
 
-    first = True
-    for b_box, type, prob_type, prob in predictions[:min(10, len(predictions))]:
-        if not first:
-            print("Probability:", prob)
-            if prob < 0.1:
-                break
-            cv.rectangle(img, (b_box[0][0], b_box[0][1]), (b_box[1][0], b_box[1][1]), (0, 255, 0), thickness=1)
-            continue
-        first = False
-        cv.rectangle(img, (b_box[0][0], b_box[0][1]), (b_box[1][0], b_box[1][1]), (255, 0, 0), thickness=3)
-        cv.putText(img, f"Class={type}, p={int(prob_type * 100)}, face={int(prob * 100)}", (b_box[0][0], b_box[0][1]),
-                cv.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 0), 1)
-
-        print("Intersection Over Reunion:", project_utils.intersection_over_reunion(
-            ((xmin, ymin), (xmax, ymax)), b_box
-        ))
-
-    plt.imshow(img)
-    plt.show()
+        for b_box, prob in preds[-1]:
+            if prob > 0.95:
+                cv.rectangle(im, (b_box[0][0], b_box[0][1]), (b_box[1][0], b_box[1][1]), (255, 0, 0), thickness=3)
+            
+        plt.imshow(im)
+        plt.show()
 
